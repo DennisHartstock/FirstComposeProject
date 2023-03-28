b3ce6d6bc61c2f35fdb9412cc23c5fd0dd45b4c2
DennsHartstock <dennis.hartstock@web.de>
2023-03-28 11:29:38 +0200

    update dependencies, @Preview and Column

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -3,26 +3,31 @@ package com.example.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//                    Greeting()
-            UserInfo(name = "Dennis", age = 46)
+            UserInfoPreview()
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting() {
-    val name = "John"
-    Text(text = "Hello $name!")
+fun UserInfoPreview() {
+    UserInfo(name = "Dennis", age = 47)
 }
 
 @Composable
 fun UserInfo(name: String, age: Int) {
-    Text(text = "Hello, $name. You are $age years old")
+    Column {
+        repeat(10) {
+            Text(text = "Hello, $name. You are $age years old")
+        }
+    }
 }
